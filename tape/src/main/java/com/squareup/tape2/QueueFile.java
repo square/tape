dfd3046c1574dd6d2e346ed78745e8ac0e612701
Paulo Costa <me@paulo.costa.nom.br>
2016-11-30 21:43:11 -0200

    fix QueueFile.readLong()

--- tape/src/main/java/com/squareup/tape2/QueueFile.java
+++ tape/src/main/java/com/squareup/tape2/QueueFile.java
@@ -268,11 +268,11 @@ public final class QueueFile implements Closeable, Iterable<byte[]> {
 
   /** Reads an {@code long} from the {@code byte[]}. */
   private static long readLong(byte[] buffer, int offset) {
-    return ((buffer[offset    ] & 0xffL) << 24)
-        +  ((buffer[offset + 1] & 0xffL) << 16)
-        +  ((buffer[offset + 2] & 0xffL) << 8)
-        +  ((buffer[offset + 3] & 0xffL) << 16)
-        +  ((buffer[offset + 4] & 0xffL) << 8)
+    return ((buffer[offset    ] & 0xffL) << 56)
+        +  ((buffer[offset + 1] & 0xffL) << 48)
+        +  ((buffer[offset + 2] & 0xffL) << 40)
+        +  ((buffer[offset + 3] & 0xffL) << 32)
+        +  ((buffer[offset + 4] & 0xffL) << 24)
         +  ((buffer[offset + 5] & 0xffL) << 16)
         +  ((buffer[offset + 6] & 0xffL) << 8)
         +   (buffer[offset + 7] & 0xffL);
