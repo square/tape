b7908b17db541db250b52cf07a7760e19980a5a3
Nikolay Nesterov <nikolaynesterov@gmail.com>
2017-04-06 23:40:38 +0300

    Add serialVersionUID to TapeException

--- tape/src/main/java/com/squareup/tape2/TapeException.java
+++ tape/src/main/java/com/squareup/tape2/TapeException.java
@@ -3,6 +3,8 @@ package com.squareup.tape2;
 /** Thrown when an internal error has occurred in tape lib **/
 public class TapeException extends RuntimeException {
 
+    private static final long serialVersionUID = 5138225684096988531L;
+
     public TapeException(String message, Exception cause) {
         super(message, cause);
     }
